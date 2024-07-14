705824602cd23dbca2b1de9086779fca2af71005
Armin Raad <armin3600z@gmail.com>
2024-07-14 09:30:49 +0100

    add fluid multiplier

--- control.lua
+++ control.lua
@@ -1,27 +1,32 @@
 function fix_resources_for_surface(surface, area)
-    resources = surface.find_entities_filtered{
-        area = area,
-        type = 'resource',
-    }
-    for _, resource in pairs(resources) do
-        if resource.prototype.infinite_resource then
-            -- set all new resources to exactly their normal amount
-            resource.initial_amount = resource.prototype.normal_resource_amount
-            resource.amount = resource.prototype.normal_resource_amount
-        end
-    end
+	resources = surface.find_entities_filtered({
+		area = area,
+		type = "resource",
+	})
+	for _, resource in pairs(resources) do
+		if resource.prototype.infinite_resource then
+			-- set all new resources to exactly their normal amount
+			local multiplier = 1
+			if resource.prototype.resource_category == "basic-fluid" then
+				multiplier = settings.global["fluid-yield-multiplier"].value
+			end
+
+			resource.initial_amount = resource.prototype.normal_resource_amount * multiplier
+			resource.amount = resource.prototype.normal_resource_amount * multiplier
+		end
+	end
 end
 
 script.on_configuration_changed(function()
-    for _, surface in pairs(game.surfaces) do
-        fix_resources_for_surface(surface)
-    end
+	for _, surface in pairs(game.surfaces) do
+		fix_resources_for_surface(surface)
+	end
 end)
 
 script.on_event(defines.events.on_chunk_generated, function(event)
-    fix_resources_for_surface(event.surface, event.area)
+	fix_resources_for_surface(event.surface, event.area)
 end)
 
 script.on_event(defines.events.on_surface_created, function(event)
-    fix_resources_for_surface(game.surfaces[event.surface_index])
+	fix_resources_for_surface(game.surfaces[event.surface_index])
 end)
---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+	{
+		type = "double-setting",
+		name = "fluid-yield-multiplier",
+		setting_type = "runtime-global",
+		minimum_value = 0.1,
+		maximum_value = 9999,
+		default_value = 1,
+	},
+})
